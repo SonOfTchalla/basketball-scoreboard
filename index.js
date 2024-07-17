f3730cb3eb7d9a6852b6684be76f80bf685dd185
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2024-07-17 15:43:33 +0200

    Add a function that will increment the score
    Create a function that will increment the score of a certain team by a certain amount depending on which button is clicked

--- index.js
+++ index.js
@@ -13,3 +13,28 @@ const p2plus3 = document.querySelector(".p2 #plus-3");
 let p1score = 0;
 let p2score = 0;
 
+function increment(selector, display){
+    selector.addEventListener('click', () => {
+        if(selector === p1plus1){
+            p1score += 1;
+            p1h1.textContent = p1score;
+        }else if(selector === p1plus2){
+            p1score += 2;
+            p1h1.textContent = p1score;
+        }else if(selector === p1plus3){
+            p1score += 3;
+            p1h1.textContent = p1score;
+        }else if(selector === p2plus1){
+            p2score += 1;
+            p2h1.textContent = p2score;
+        }else if(selector === p2plus2){
+            p2score += 2;
+            p2h1.textContent = p2score;
+        }else if(selector === p2plus3){
+            p2score += 3;
+            p2h1.textContent = p2score;
+        }else{
+            return 0;
+        }
+    });
+}
