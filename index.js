450edda6e82893c23b904deeb0d8ee795d516c5c
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2024-07-17 15:57:21 +0200

    Add comments and spaces for better readabilty

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
+//select the two h1 displaying each team's score
 const p1h1 = document.querySelector(".p1 h1");
 const p2h1 = document.querySelector(".p2 h1");
 
+//select Team 1's buttons
 const p1plus1 = document.querySelector(".p1 #plus-1");
-const p2plus1 = document.querySelector(".p2 #plus-1");
-
 const p1plus2 = document.querySelector(".p1 #plus-2");
-const p2plus2 = document.querySelector(".p2 #plus-2");
-
 const p1plus3 = document.querySelector(".p1 #plus-3");
+
+//select Team 1's buttons
+const p2plus1 = document.querySelector(".p2 #plus-1");
+const p2plus2 = document.querySelector(".p2 #plus-2");
 const p2plus3 = document.querySelector(".p2 #plus-3");
 
+//Instantiate each team's score
 let p1score = 0;
 let p2score = 0;
 
+//function declaration
 function increment(selector, display){
     selector.addEventListener('click', () => {
         if(selector === p1plus1){
@@ -39,9 +43,13 @@ function increment(selector, display){
     });
 };
 
+
+//function calls for team 1
 increment(p1plus1, p1h1);
 increment(p1plus2, p1h1);
 increment(p1plus3, p1h1);
+
+//function calls for team 2
 increment(p2plus1, p2h1);
 increment(p2plus2, p2h1);
 increment(p2plus3, p2h1);
