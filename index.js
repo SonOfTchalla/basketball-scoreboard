ddbefda2a7292e3c1c4c056f91f16d385f377301
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2024-07-17 14:32:39 +0200

    Add querySelector for the h1 and buttons
    Use variables to select the two h1 displaying the score and the six buttons used to increase the score

---
+++ index.js
@@ -0,0 +1,12 @@
+const p1h1 = document.querySelector(".p1 h1");
+const p2h1 = document.querySelector(".p1 h1");
+
+const p1plus1 = document.querySelector(".p1 #plus-1");
+const p2plus1 = document.querySelector(".p2 #plus-1");
+
+const p1plus2 = document.querySelector(".p1 #plus-2");
+const p2plus2 = document.querySelector(".p2 #plus-2");
+
+const p1plus3 = document.querySelector(".p1 #plus-3");
+const p2plus3 = document.querySelector(".p2 #plus-3");
+
