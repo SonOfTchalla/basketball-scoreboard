e41d41e1e9d8f4a5a809e9e5a1e0aebe00def2b6
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2024-07-17 14:39:49 +0200

    Add functionality for the Home Team's buttons
    The '+1' button wil increase the home team's score by 1, the '+2' button by 2 and the '+3' button by 3

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ const p2plus2 = document.querySelector(".p2 #plus-2");
 const p1plus3 = document.querySelector(".p1 #plus-3");
 const p2plus3 = document.querySelector(".p2 #plus-3");
 
+let score = 0;
+
+p1plus1.addEventListener('click', () => {
+    score += 1;
+    p1h1.textContent = score;
+})
+
+p1plus2.addEventListener('click', () => {
+    score += 2;
+    p1h1.textContent = score;
+})
+
+p1plus3.addEventListener('click', () => {
+    score += 3;
+    p1h1.textContent = score;
+})
